c4f31e9bf8149cd01d791f5c927db71728a2bee2
James Dominy <jgdominy@gmail.com>
2015-04-16 10:31:09 +0200

    Adds required stuff to setup.py for upload to PyPI (Fixes #3)

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from distutils.core import setup, Extension
+from setuptools import setup, Extension
 
 ringbuf = Extension('ringbuf', sources=["src/ringbuf.c"])
 
@@ -6,5 +6,19 @@ setup(
     name = "Ring Buffer",
     version = "0.1",
     description = "A circular/ring buffer written in C",
-    ext_modules = [ringbuf]
+    ext_modules = [ringbuf],
+    url = "https://github.com/sirlark/pyringbuf",
+    author = "James Dominy",
+    license = "MIT",
+    classifiers = [
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: C",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: Implementation :: CPython"
+    ],
+    keywords = "circular ring buffer",
+    test_suite = "test.tests"
 )
